32d11c2655d6dacabd9e6eb09cdc98197ffce291
Bhavya <bhavyab.1712@gmail.com>
2022-02-20 21:52:54 +0530

    Commit

---
+++ src/com/cg/hello/HelloWorld.java
@@ -0,0 +1,47 @@
+package com.cg.hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		boolean a = true;
+		if(a==true) {
+			System.out.println("Hello");
+			
+		}
+		
+		byte b =10;
+		System.out.println(b);
+		
+		b++;
+		System.out.println(b);
+		
+		++b;
+		System.out.println(b);
+		
+		char c = 'H';
+		System.out.println(c);
+		
+		int i = 100;
+		System.out.println(i);
+		
+		float f = 6.777f;
+		System.out.println(f);
+		
+		double d = 6.7809578;
+		System.out.println(d);
+		
+		byte by = (byte) d;
+		System.out.println(by);
+
+		int x = 10;
+		byte y = (byte) x;
+		System.out.println(y);
+		
+		int r = (int) d;
+		
+		
+	}
+
+}
